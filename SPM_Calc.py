1868f2de72b02720571d13072109d9fbc95bedd2
nadira <nadirayasmin30062005@gmail.com>
2024-05-15 01:17:15 +0800

    a new window opens when button is pressed

--- SPM_Calc.py
+++ SPM_Calc.py
@@ -8,7 +8,16 @@
 4. Kalau nak merge dgn main code, buat branch baru sbb takut tk jadi . so apa apa boleh pergi balik branch asal
 5. git push -u origin branchname
 '''
-import tkinter
+import tkinter 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+def open_form_4_window():
+    form_4_window= tk.Toplevel()
+
+def open_form_5_window():
+    form_5_window= tk.Toplevel()
 
 if __name__ == '__main__':
     window=tkinter.Tk()
@@ -16,18 +25,19 @@ if __name__ == '__main__':
     window.geometry('600x400')
     window.configure(bg='#0317fc')
 
-    frame =tkinter.Frame(window)
-    frame.pack()
+    frame =tkinter.Frame(bg='#0317fc')
+    
 
     #widgets
-    label_spmcalculator = tkinter.Label (frame, text='spm calculator', bg='#FFFFFF', fg="#0317fc")
-    form4_button = tkinter.Button (frame, text='form 4', bg='#FFFFFF', fg="#0317fc")
-    form5_button = tkinter.Button (frame, text='form 5', bg='#FFFFFF',fg='#0317fc')
+    label_spmcalculator = tkinter.Label (frame, text='spm calculator', bg='#0317fc', fg="#FFFFFF" , font='Arial, 30', pady=40)
+    form4_button = tkinter.Button (frame, text='form 4', bg='#FFFFFF', fg="#0317fc", font='Arial, 20', padx=20, command=open_form_4_window)
+    form5_button = tkinter.Button (frame, text='form 5', bg='#FFFFFF',fg='#0317fc', font='Arial, 20', padx=20, command=open_form_5_window)
 
     #placing them
-    label_spmcalculator.grid(row=0, column=0, columnspan=2)
+    label_spmcalculator.grid(row=0, column=0, columnspan=2, sticky='news')
     form4_button.grid(row=1, column=0)
     form5_button.grid(row=1, column=1)
+    frame.pack()
 
 
     window.mainloop()
