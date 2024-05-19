9b313894708f94bfa1cdef19b9378b32aa2e7e21
nadira <nadirayasmin30062005@gmail.com>
2024-05-19 15:23:25 +0800

    centered the widgets

--- SPM_Calc.py
+++ SPM_Calc.py
@@ -24,13 +24,13 @@ if __name__ == '__main__':
 
     window.title("SPM Calculator")
     window.geometry('600x400')
-    window.configure(bg='#0317fc')
+    window.configure(bg='#000000')
 
-    frame = tk.Frame(window, bg='#0317fc')
+    frame = ttk.Frame(window)
     frame.grid(row=0, column=0, sticky='news')
     
     # Widgets
-    label_spmcalculator = tk.Label(frame, text='SPM Calculator', bg='#0317fc', fg="#FFFFFF", font=('Arial', 30), pady=40)
+    label_spmcalculator = tk.Label(frame, text='SPM Calculator', bg='#0317fc', fg="#000000", font=('Arial', 30), pady=40)
     form4_button = tk.Button(frame, text='Form 4', bg='#FFFFFF', fg="#0317fc", font=('Arial', 20), padx=20, command=lambda: form_4_window.tkraise())
     form5_button = tk.Button(frame, text='Form 5', bg='#FFFFFF', fg='#0317fc', font=('Arial', 20), padx=20, command=lambda: form_5_window.tkraise())
     back_button_form4 = tk.Button(form_4_window, text='Back', command=lambda: frame.tkraise())
@@ -40,10 +40,28 @@ if __name__ == '__main__':
     form4_button.grid(row=1, column=0)
     form5_button.grid(row=1, column=1)
     
+    
     back_button_form4.grid(row=0, column=0)
     back_button_form5.grid(row=0, column=0)
+
+    label_spmcalculator.grid(row=0, column=0, columnspan=2, pady=10)
+    form4_button.grid(row=1, column=0, padx=10, pady=10)
+    form5_button.grid(row=1, column=1, padx=10, pady=10)
     
-    frame.grid(row=0, column=0, sticky='news')
+    back_button_form4.grid(row=1, column=0, padx=10, pady=10)
+    back_button_form5.grid(row=1, column=0, padx=10, pady=10)
+
+    frame.grid_columnconfigure(0, weight=1)
+    frame.grid_columnconfigure(1, weight=1)
+
+    form_4_window.grid_columnconfigure(0, weight=1)
+    form_4_window.grid_rowconfigure(0, weight=1)
+    form_5_window.grid_columnconfigure(0, weight=1)
+    form_5_window.grid_rowconfigure(0, weight=1)
+
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_rowconfigure(0, weight=1)
+    
     frame.tkraise()
 
     window.mainloop()
