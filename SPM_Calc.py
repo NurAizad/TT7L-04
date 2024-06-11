b496f38277bdf6924b53a78b2cd6d747e39124df
Aizad <1221109241@student.mmu.edu.my>
2024-06-11 19:58:43 +0800

    added chem(danysh) calculator

--- SPM_Calc.py
+++ SPM_Calc.py
@@ -27,7 +27,7 @@ window.configure(bg='#212129')
 screenWidth=window.winfo_screenwidth() #utk dptkan width window
 screenHeight=window.winfo_screenheight()#dptkan height
 
-BgImage=Image.open("images/science.jpg")
+BgImage=Image.open("images/class.jpg")
 ResizeBgImage= BgImage.resize((screenWidth,screenHeight))
 FinalBgImage=ImageTk.PhotoImage(ResizeBgImage)
 BgImage_label=tk.Label(window,image=FinalBgImage)
@@ -42,9 +42,6 @@ chem_frame=tk.Frame(bg="#212129")
 bio_frame=tk.Frame(bg="#212129")
 admin_frame=tk.Frame(bg="#212129")
 adminInterface_frame=tk.Frame(bg="#212129")
-#adminSearch_frame=tk.Frame(bg="#212129")
-#adminDisplay_frame=tk.Frame(bg="#212129")
-#adminButtons_frame=tk.Frame(bg="#212129")
 
 #physics
 phy_frame=tk.Frame(bg="#212129")
@@ -59,7 +56,11 @@ impulse_frame=tk.Frame(bg="#212129")
 impulsiveForce_frame=tk.Frame(bg="#212129")
 weight_frame=tk.Frame(bg="#212129")
 
-
+#chemistry
+chem_frame=tk.Frame(bg="#212129")
+chemf4_frame=tk.Frame(bg="#212129")
+chemf4c2_frame=tk.Frame(bg="#212129")
+nucleon_frame=tk.Frame(bg="#212129")
 
 
 def register(): #function utk kalau click register keluar page register
@@ -182,12 +183,12 @@ def login(): #ni nanti kena connect dgn database sql kot
             subject_label = tk.Label(subject_frame, text="Choose a subject", bg="#212129",fg="#08edff", font=("Helvetica",34))
 
             phy_button = tk.Button(subject_frame, text="Physics", bg="#90ee90", font=("Helvetica",24),command=lambda:PhysicsPage(subject_frame,phy_frame))
-            chem_button = tk.Button(subject_frame, text="Chemistry", bg="#add8e6", font=("Helvetica",24))
+            chem_button = tk.Button(subject_frame, text="Chemistry", bg="#add8e6", font=("Helvetica",24),command=lambda:ChemistryPage(subject_frame,chem_frame))
             bio_button = tk.Button(subject_frame, text="Biology", bg="#f08080", font=("Helvetica",24))
 
             subject_back = tk.Button(subject_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(subject_frame,frame))
 
-            subject_label.grid(row=0,column=1,columnspan=2,sticky="news",padx=20,pady=40)
+            subject_label.grid(row=0,column=1,columnspan=2,sticky="news",padx=50,pady=40)
             subject_back.grid(row=6,column=1,columnspan=2,padx=20,pady=40)
             phy_button.grid(row=1,column=1,columnspan=2,padx=20,pady=20,sticky="ew")
             chem_button.grid(row=2,column=1,columnspan=2,padx=20,pady=20,sticky="ew")
@@ -199,7 +200,7 @@ def login(): #ni nanti kena connect dgn database sql kot
             subject_label = tk.Label(admin_frame, text="Choose a subject", bg="#212129",fg="#08edff", font=("Helvetica",34))
 
             phy_button = tk.Button(admin_frame, text="Physics", bg="#90ee90", font=("Helvetica",24),command=lambda:PhysicsPage(admin_frame,phy_frame))
-            chem_button = tk.Button(admin_frame, text="Chemistry", bg="#add8e6", font=("Helvetica",24))
+            chem_button = tk.Button(admin_frame, text="Chemistry", bg="#add8e6", font=("Helvetica",24),command=lambda:ChemistryPage(admin_frame,chem_frame))
             bio_button = tk.Button(admin_frame, text="Biology", bg="#f08080", font=("Helvetica",24))
             admin_button = tk.Button(admin_frame, text="Admin", bg="#b16eeb", font=("Helvetica",24),command=lambda:admin())
 
@@ -573,6 +574,87 @@ def PhysicsPage(forget_surface,pack_surface):
     form5_button.grid(row=1,column=2,padx=20)
     chooseForm_label.grid(row=0,column=1,sticky="ew",padx=10,pady=10)
     phy_back.grid(row=6,column=1,pady=40)
+
+def ChemistryPage(forget_surface, pack_surface):
+    backbutton(forget_surface, pack_surface)
+    def chem_f4():
+        backbutton(chem_frame,chemf4_frame)
+        def chem_f4_chap2():
+            backbutton(chemf4_frame,chemf4c2_frame)
+            def nucleon_number():
+                backbutton(chemf4c2_frame, nucleon_frame)
+                def nucleon_calc():
+                    number_of_protons=int(number_of_protons_entry.get())
+                    number_of_neutrons=int(number_of_neutrons_entry.get())
+                    if number_of_protons <0 or number_of_neutrons <0:
+                        messagebox.showerror(title="Error", message="Number of protons and number of neutrons cannot be negative")
+                    else:
+                        nucleon=number_of_protons + number_of_neutrons
+                    result_label.config(text=f"{nucleon}")
+
+
+                nucleon_label=tk.Label(nucleon_frame, text="Nucleon Number", bg="#212129",fg="#08edff",font=("Helvetica",34))
+                number_of_protons_label=tk.Label(nucleon_frame,text="Number of Protons",bg="#212129",fg="#90ee90", font=("Helvetica", 24))
+                number_of_neutrons_label=tk.Label(nucleon_frame,text="Number of Neutrons",bg="#212129",fg="#90ee90", font=("Helvetica", 24))
+                result_label=tk.Label(nucleon_frame,text="",bg="#212129",fg="#FFFFFF", font=("Helvetica", 24))
+
+                number_of_protons_entry=tk.Entry(nucleon_frame, font=("Helvetica", 16))
+                number_of_neutrons_entry=tk.Entry(nucleon_frame, font=("Helvetica", 16))
+
+                nucleon_back=tk.Button(nucleon_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton_delresult(nucleon_frame,chemf4c2_frame,result_label))
+                nucleon_back.grid(row=9,column=1,pady=10)
+                calculate_button=tk.Button(nucleon_frame,text="Calculate",bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16),command=lambda:nucleon_calc())
+                calculate_button.grid(row=8,column=1,pady=10)
+
+                nucleon_label.grid(row=0,column=1,pady=10,padx=20)
+                number_of_protons_label.grid(row=1,column=0,pady=10,padx=20)
+                number_of_neutrons_label.grid(row=2,column=0,pady=10,padx=50)
+
+                number_of_protons_entry.grid(row=1,column=2,padx=20)
+                number_of_neutrons_entry.grid(row=2,column=2,padx=20)
+                result_label.grid(row=3,column=2,padx=20)
+
+            f4Chapter2_label=tk.Label(chemf4c2_frame, text="Physics Form 4 Chapter 2", bg="#212129",fg="#08edff",font=("Helvetica",34))
+            nucleon_number_button=tk.Button(chemf4c2_frame,text="Speed",bg="#90ee90", font=("Helvetica",24),command=lambda:nucleon_number())
+            
+
+            chem_f4c2_back=tk.Button(chemf4c2_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(chemf4c2_frame,chemf4_frame))
+            chem_f4c2_back.grid(row=9,column=1,pady=10)
+
+
+            f4Chapter2_label.grid(row=0,column=1,padx=20,pady=10)
+            nucleon_number_button.grid(row=1,column=1,pady=10,sticky="ew",padx=20)          
+                    
+
+        f4Chapter_label=tk.Label(chemf4_frame, text="Physics Form 4", bg="#212129",fg="#08edff",font=("Helvetica",34))
+        chap2_button=tk.Button(chemf4_frame,text="Chapter 2",bg="#90ee90", font=("Helvetica",24),command=lambda:chem_f4_chap2())
+        chap3_button=tk.Button(chemf4_frame,text="Chapter 3",bg="#adb0b4", font=("Helvetica",24))
+        chap4_button=tk.Button(chemf4_frame,text="Chapter 4",bg="#adb0b4", font=("Helvetica",24))
+        chap5_button=tk.Button(chemf4_frame,text="Chapter 5",bg="#adb0b4", font=("Helvetica",24))
+        chap6_button=tk.Button(chemf4_frame,text="Chapter 6",bg="#adb0b4", font=("Helvetica",24))
+
+        chem_f4_back=tk.Button(chemf4_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(chemf4_frame,pack_surface))
+        chem_f4_back.grid(row=6,column=2,pady=40)
+
+        f4Chapter_label.grid(row=0,column=2,padx=10,pady=10)
+        chap2_button.grid(row=1,column=0,padx=20,pady=50)
+        chap3_button.grid(row=1,column=2,padx=20)
+        chap4_button.grid(row=1,column=4,padx=20)
+        chap5_button.grid(row=2,column=1,padx=20,pady=50)
+        chap6_button.grid(row=2,column=3,padx=20)
+    
+    
+    form4_button=tk.Button(chem_frame,text="Form 4", bg="#90ee90", font=("Helvetica",24), command=lambda:chem_f4())
+    form5_button=tk.Button(chem_frame,text="Form 5", bg="#adb0b4", font=("Helvetica",24))
+    chooseForm_label=tk.Label(chem_frame,text="Choose which form:",bg="#212129",fg="#08edff", font=("Helvetica",34))
+
+    chem_back = tk.Button(chem_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(pack_surface,forget_surface))
+
+    form4_button.grid(row=1,column=0,padx=20,pady=100)
+    form5_button.grid(row=1,column=2,padx=20)
+    chooseForm_label.grid(row=0,column=1,sticky="ew",padx=10,pady=10)
+    chem_back.grid(row=6,column=1,pady=40)
+
 
 def admin():
         #global cursor
