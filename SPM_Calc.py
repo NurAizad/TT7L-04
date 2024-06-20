c07811ef973cf2f45df79c0d74f6da3eab408b22
Aizad <1221109241@student.mmu.edu.my>
2024-06-20 15:17:23 +0800

    added danysh code

--- SPM_Calc.py
+++ SPM_Calc.py
@@ -60,6 +60,10 @@ f4_chem_frame=tk.Frame(bg="#212129")
 f4_chemf4_frame=tk.Frame(bg="#212129")
 f4_chemf4c2_frame=tk.Frame(bg="#212129")
 f4_nucleon_frame=tk.Frame(bg="#212129")
+f4_chemf4c6_frame=tk.Frame(bg="#212129")
+f4_pH_frame=tk.Frame(bg='#212129')
+f4_concentration_frame=tk.Frame(bg="#212129")
+f4_molarity_frame=tk.Frame(bg="#212129")
 
 f4_bio_frame=tk.Frame(bg="#212129")
 f4_biof4_frame=tk.Frame(bg="#212129")
@@ -95,10 +99,18 @@ chem_frame=tk.Frame(bg="#212129")
 chemf4_frame=tk.Frame(bg="#212129")
 chemf4c2_frame=tk.Frame(bg="#212129")
 nucleon_frame=tk.Frame(bg="#212129")
+chemf4c6_frame=tk.Frame(bg="#212129")
+pH_frame=tk.Frame(bg='#212129')
+concentration_frame=tk.Frame(bg="#212129")
+molarity_frame=tk.Frame(bg="#212129")
 
+chemf5_frame=tk.Frame(bg="#212129")
+chemf5c1_frame=tk.Frame(bg="#212129")
+Eocell_frame=tk.Frame(bg="#212129")
 chemf5_frame=tk.Frame(bg="#212129")
 chemf5c3_frame=tk.Frame(bg="#212129")
 fuelValue_frame=tk.Frame(bg="#212129")
+
 #biology
 bio_frame=tk.Frame(bg="#212129")
 biof4_frame=tk.Frame(bg="#212129")
@@ -742,7 +754,7 @@ def PhysicsPage(forget_surface,pack_surface):
                 result_label.grid(row=2,column=1,pady=10)
             f5Chapter3_label=tk.Label(phyf4c2_frame, text="Physics Form 5 Chapter 3", bg="#212129",fg="#08edff",font=("Helvetica",34))
             f5Chapter3_label.grid(row=0,column=1,pady=10)
-            weight_button=tk.Button(phyf5c3_frame,text="weight",bg="#90ee90", font=("Helvetica",24),command=lambda:weight())
+            weight_button=tk.Button(phyf5c3_frame,text="Weight",bg="#90ee90", font=("Helvetica",24),command=lambda:weight())
             weight_button.grid(row=1,column=1,pady=10,sticky="ew")   
             backbutton_weight_c3 = tk.Button(phyf5c3_frame, text='Back',  bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda: backbutton(phyf5c3_frame, phyf5_frame)) 
             backbutton_weight_c3.grid(row=6,column=1)
@@ -801,8 +813,8 @@ def ChemistryPage(forget_surface, pack_surface):
 
 
                 nucleon_label=tk.Label(nucleon_frame, text="Nucleon Number", bg="#212129",fg="#08edff",font=("Helvetica",34))
-                number_of_protons_label=tk.Label(nucleon_frame,text="Number of Protons",bg="#212129",fg="#90ee90", font=("Helvetica", 24))
-                number_of_neutrons_label=tk.Label(nucleon_frame,text="Number of Neutrons",bg="#212129",fg="#90ee90", font=("Helvetica", 24))
+                number_of_protons_label=tk.Label(nucleon_frame,text="Number of Protons",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
+                number_of_neutrons_label=tk.Label(nucleon_frame,text="Number of Neutrons",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
                 result_label=tk.Label(nucleon_frame,text="",bg="#212129",fg="#FFFFFF", font=("Helvetica", 24))
 
                 number_of_protons_entry=tk.Entry(nucleon_frame, font=("Helvetica", 16))
@@ -832,28 +844,110 @@ def ChemistryPage(forget_surface, pack_surface):
             f4Chapter2_label.grid(row=0,column=1,padx=20,pady=10)
             nucleon_number_button.grid(row=1,column=1,pady=10,sticky="ew",padx=20)          
                     
+        def chem_f4_chap6():
+            backbutton(chemf4_frame,chemf4c6_frame)
+            def pH():
+                backbutton(chemf4c6_frame,pH_frame)
+                def pH_calc():
+                    pH_value=float(pH_value_entry.get())
+                    if pH_value <0:
+                        messagebox.showerror(title="Error", message="pH value cannot be negative")
+                    else:
+                        pH = -math.log10(pH_value)
+                    result_label.config(text=f"{pH}")
+                pH_label=tk.Label(pH_frame, text='pH Value',bg="#212129",fg="#08edff",font=("Helvetica",34) ) 
+                pH_value_label=tk.Label(pH_frame,text='H ions',bg="#212129",fg="#add8e6",font=("Helvetica",34))
+                result_label=tk.Label(pH_frame,text="",bg="#212129",fg="#FFFFFF", font=("Helvetica", 24))
+                pH_value_entry=tk.Entry(pH_frame, font=("Helvetica", 16))
+                pH_back=tk.Button(pH_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton_delresult(pH_frame,chemf4c6_frame,result_label))
+                pH_back.grid(row=9, column=1, pady=10)
+                calculate_button=tk.Button(pH_frame,text="Calculate",bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16),command=lambda:pH_calc())
+                calculate_button.grid(row=8,column=1,pady=10)
+                pH_label.grid(row=0,column=1,pady=10)
+                pH_value_label.grid(row=1,column=0,pady=10,padx=70)
+                pH_value_entry.grid(row=1,column=2,padx=20)
+                result_label.grid(row=3,column=1,pady=10,padx=20)
+
+            def concentration():
+                backbutton(chemf4c6_frame,concentration_frame)
+                def concentration_calc():
+                    mass_solute=float(mass_solute_entry.get())
+                    volume_solution=float(volume_solution_entry.get())
+                    if volume_solution<0:
+                        messagebox.showerror(title="Error", message="Volume of solution cannot be negative")
+                    else:
+                        concentration=mass_solute/volume_solution
+                    result_label.config(text=f"{concentration} g d/m^3")
+
+                concentration_label=tk.Label(concentration_frame, text="Concentration", bg="#212129",fg="#08edff",font=("Helvetica",34))
+                mass_solute_label=tk.Label(concentration_frame,text="Mass of Solute",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
+                volume_solution_label=tk.Label(concentration_frame,text="Volume of Solution",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
+                result_label=tk.Label(concentration_frame,text="",bg="#212129",fg="#FFFFFF", font=("Helvetica", 24))
+                mass_solute_entry=tk.Entry(concentration_frame, font=("Helvetica", 16))
+                volume_solution_entry=tk.Entry(concentration_frame, font=("Helvetica", 16))
+                concentration_back=tk.Button(concentration_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton_delresult(concentration_frame,chemf4c6_frame,result_label))
+                concentration_back.grid(row=9,column=1,pady=10)
+                calculate_button=tk.Button(concentration_frame,text="Calculate",bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16),command=lambda:concentration_calc())
+                calculate_button.grid(row=8,column=1,pady=10)
+                concentration_label.grid(row=0,column=1,pady=10,padx=20)
+                mass_solute_label.grid(row=1,column=0,pady=10,padx=10)
+                volume_solution_label.grid(row=2,column=0,pady=10,padx=10)
+                mass_solute_entry.grid(row=1,column=2,padx=20)
+                volume_solution_entry.grid(row=2,column=2,padx=20)
+                result_label.grid(row=3,column=1,padx=20)
+
+            def molarity():
+                backbutton(chemf4c6_frame,molarity_frame)
+                def molarity_calc():
+                    number_moles_solute=float(number_moles_solute_entry.get())
+                    volume_solution=float(volume_solution_entry.get())
+                    if volume_solution<0:
+                        messagebox.showerror(title="Error", message="Volume of solution cannot be negative")
+                    else:
+                        molarity=number_moles_solute/volume_solution
+                    result_label.config(text=f"{molarity} mol d/m^3")
+
+                molarity_label=tk.Label(molarity_frame, text="Molarity", bg="#212129",fg="#08edff",font=("Helvetica",34))
+                number_moles_solute_label=tk.Label(molarity_frame,text="Number of moles of solute",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
+                volume_solution_label=tk.Label(molarity_frame,text="Volume of Solution",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
+                result_label=tk.Label(molarity_frame,text="",bg="#212129",fg="#FFFFFF", font=("Helvetica", 24))
+                number_moles_solute_entry=tk.Entry(molarity_frame, font=("Helvetica", 16))
+                volume_solution_entry=tk.Entry(molarity_frame, font=("Helvetica", 16))
+                molarity_back=tk.Button(molarity_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton_delresult(molarity_frame,chemf4c6_frame,result_label))
+                molarity_back.grid(row=9,column=1,pady=10)
+                calculate_button=tk.Button(molarity_frame,text="Calculate",bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16),command=lambda:molarity_calc())
+                calculate_button.grid(row=8,column=1,pady=10)
+                molarity_label.grid(row=0,column=1,pady=10,padx=20)
+                number_moles_solute_label.grid(row=1,column=0,pady=10,padx=10)
+                volume_solution_label.grid(row=2,column=0,pady=10,padx=10)
+                number_moles_solute_entry.grid(row=1,column=2,padx=20)
+                volume_solution_entry.grid(row=2,column=2,padx=20)
+                result_label.grid(row=3,column=1,padx=20)
+
+            f4Chapter6_label=tk.Label(chemf4c6_frame, text="Chemsitry Form 4 Chapter 6", bg="#212129",fg="#08edff",font=("Helvetica",34))
+            pH_button=tk.Button(chemf4c6_frame,text="pH Value",bg="#add8e6", font=("Helvetica",24),command=lambda:pH())
+            concentration_button=tk.Button(chemf4c6_frame, text="Concentration",bg='#add8e6', font=("Helvetica", 24),command=lambda:concentration())
+            molarity_button=tk.Button(chemf4c6_frame,text="Molarity",bg="#add8e6", font=("Helvetica",24),command=lambda:molarity())
+
+            chem_f4c6_back=tk.Button(chemf4c6_frame, text='Back' , bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16),command=lambda:backbutton(chemf4c6_frame,chemf4_frame))
+            chem_f4c6_back.grid(row=9,column=1,pady=10)
+
+            f4Chapter6_label.grid(row=0,column=1,padx=20,pady=10)
+            pH_button.grid(row=1,column=1,pady=10,padx=20,sticky="ew")  
+            concentration_button.grid(row=2,column=1,pady=10,sticky="ew",padx=20)     
+            molarity_button.grid(row=3,column=1,pady=10,sticky="ew",padx=20)
 
         f4Chapter_label=tk.Label(chemf4_frame, text="Chemistry Form 4", bg="#212129",fg="#08edff",font=("Helvetica",34))
         chap2_button=tk.Button(chemf4_frame,text="Chapter 2",bg="#add8e6", font=("Helvetica",24),command=lambda:chem_f4_chap2())
-        chap3_button=tk.Button(chemf4_frame,text="Chapter 3",bg="#adb0b4", font=("Helvetica",24))
-        chap4_button=tk.Button(chemf4_frame,text="Chapter 4",bg="#adb0b4", font=("Helvetica",24))
-        chap5_button=tk.Button(chemf4_frame,text="Chapter 5",bg="#adb0b4", font=("Helvetica",24))
-        chap6_button=tk.Button(chemf4_frame,text="Chapter 6",bg="#adb0b4", font=("Helvetica",24))
-        chap7_button=tk.Button(chemf4_frame,text="Chapter 7",bg="#adb0b4", font=("Helvetica",24))
-        chap8_button=tk.Button(chemf4_frame,text="Chapter 8",bg="#adb0b4", font=("Helvetica",24))
-        
+        chap6_button=tk.Button(chemf4_frame,text="Chapter 6",bg="#add8e6", font=("Helvetica",24),command=lambda:chem_f4_chap6())
 
         chem_f4_back=tk.Button(chemf4_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(chemf4_frame,pack_surface))
         chem_f4_back.grid(row=6,column=2,pady=40)
 
         f4Chapter_label.grid(row=0,column=2,padx=10,pady=10)
-        chap2_button.grid(row=1,column=0,padx=20,pady=50)
-        chap3_button.grid(row=1,column=2,padx=20)
-        chap4_button.grid(row=1,column=4,padx=20)
-        chap5_button.grid(row=2,column=0,padx=20,pady=50)
-        chap6_button.grid(row=2,column=2,padx=20)
-        chap7_button.grid(row=2,column=4,padx=20)
-        chap8_button.grid(row=3,column=2,padx=20,pady=50)
+        chap2_button.grid(row=1,column=2,padx=20,pady=50)
+        chap6_button.grid(row=2,column=2,padx=20,pady=50)
+
     
     def chem_f5():
         backbutton(chem_frame,chemf5_frame)
@@ -900,7 +994,57 @@ def ChemistryPage(forget_surface, pack_surface):
 
 
             f5Chapter3_label.grid(row=0,column=1,padx=20,pady=10)
-            fuelValue_button.grid(row=1,column=1,pady=10,sticky="ew",padx=20)          
+            fuelValue_button.grid(row=2,column=1,pady=10,sticky="ew",padx=20)          
+
+        def chem_f5_chap1():
+            backbutton(chemf5_frame,chemf5c1_frame)
+            f5Chapter1_label=tk.Label(chemf5c1_frame, text='Chemistry form 5 chapter 1 ', bg="#212129",fg="#08edff",font=("Helvetica",34))
+            f5Chapter1_label.grid(row=0,column=1,pady=10,padx=20)
+            Eocell_button=tk.Button(chemf5c1_frame, text='Cell Voltage', bg="#f08080", font=("Helvetica",24), command=lambda:Eocell())
+            Eocell_button.grid(row=1,column=1,pady=10,padx=20,sticky="ew")
+            chem_f5c1_back=tk.Button(biof4c4_frame, text='Back', bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16) ,command=lambda:backbutton(chemf5c1_frame,chemf4_frame) )
+            chem_f5c1_back.grid(row=9,column=1,pady=10)
+            def Eocell():
+                backbutton(chemf5c1_frame,Eocell_frame)
+                def Eocell_calc():
+                    Eo_cathode=float(Eo_cathode_entry.get())
+                    Eo_anode=float(Eo_anode_entry.get())
+                    if Eo_cathode <0:
+                        messagebox.showerror(title="Error", message="Eo cathode cannot be negative")
+                    else:
+                        Eo_cell= Eo_cathode - Eo_anode
+                    result_label.config(text=f"{Eo_cell} V")
+                Eocell_label=tk.Label(Eocell_frame, text="Cell Voltage",bg="#212129",fg="#08edff",font=("Helvetica",34))
+                Eo_cathode_label= tk.Label(Eocell_frame,text="Eo cathode",bg="#212129",fg="#add8e6", font=("Helvetica", 24))
+                Eo_anode_label = tk.Label(Eocell_frame,text="Eo anode",bg="#212129",fg="#add8e6", font=("Helvetica", 24)) 
+                result_label = tk.Label(Eocell_frame,text="",bg="#212129",fg="#FFFFFF", font=("Helvetica", 24))
+                Eo_cathode_entry = tk.Entry(Eocell_frame, font=("Helvetica", 16))
+                Eo_anode_entry = tk.Entry(Eocell_frame, font=("Helvetica", 16))
+                Eocell_back = tk.Button(Eocell_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton_delresult(Eocell_frame,chemf5c1_frame,result_label))
+                Eocell_back.grid(row=9,column=1,pady=10)
+                calculate_button = tk.Button(Eocell_frame, text='Calculate',bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16),command=lambda:Eocell_calc())
+                calculate_button.grid(row=8,column=1,pady=10)
+                Eocell_label.grid(row=0, column=1,pady=10)
+                Eo_cathode_label.grid(row=1,column=0, pady=10,padx=20)
+                Eo_anode_label.grid(row=2, column=0,pady=10,padx=20)
+                Eo_cathode_entry.grid(row=1,column=2,padx=20)
+                Eo_anode_entry.grid(row=2, column=2,padx=20)
+                result_label.grid(row=3,column=1,pady=10,padx=20)
+            
+            f5Chapter1_label=tk.Label(chemf5c1_frame, text="Chemistry Form 5 Chapter 1", bg="#212129",fg="#08edff",font=("Helvetica",34))
+            Eocell_button=tk.Button(chemf5c1_frame,text="Cell Voltage",bg="#add8e6", font=("Helvetica",24),command=lambda:Eocell())
+            
+            chem_f5c1_back=tk.Button(chemf5c1_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(chemf5c1_frame,chemf5_frame))
+            chem_f5c1_back.grid(row=9,column=1,pady=10)
+            f5Chapter1_label.grid(row=0,column=1,padx=20,pady=10)
+            Eocell_button.grid(row=1,column=1,pady=10,sticky="ew",padx=20) 
+
+        chem_f5_back=tk.Button(chemf5_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(chemf5_frame,pack_surface))
+        chem_f5_back.grid(row=6,column=2,pady=40)
+        f5Chapter_label=tk.Label(chemf5_frame, text="Chemistry Form 5", bg="#212129",fg="#08edff",font=("Helvetica",34))
+        chap1_button=tk.Button(chemf5_frame,text="Chapter 1",bg="#add8e6", font=("Helvetica",24),command=lambda:chem_f5_chap1())
+        f5Chapter_label.grid(row=0,column=2,padx=10,pady=10)
+        chap1_button.grid(row=1,column=2,padx=20,pady=50)
 
         chem_f5_back=tk.Button(chemf5_frame, text="Back", bg="#1c6cc0", fg="#FFFFFF", font=("Helvetica", 16), command=lambda:backbutton(chemf5_frame,pack_surface))
         chem_f5_back.grid(row=6,column=2,pady=40)
@@ -909,7 +1053,7 @@ def ChemistryPage(forget_surface, pack_surface):
         chap3_button=tk.Button(chemf5_frame,text="Chapter 3",bg="#add8e6", font=("Helvetica",24),command=lambda:chem_f5_chap3())
 
         f5Chapter_label.grid(row=0,column=2,padx=10,pady=10)
-        chap3_button.grid(row=1,column=2,padx=20,pady=50)
+        chap3_button.grid(row=2,column=2,padx=20,pady=50)
 
     form4_button=tk.Button(chem_frame,text="Form 4", bg="#add8e6", font=("Helvetica",24), command=lambda:chem_f4())
     form5_button=tk.Button(chem_frame,text="Form 5", bg="#add8e6", font=("Helvetica",24), command=lambda:chem_f5())
